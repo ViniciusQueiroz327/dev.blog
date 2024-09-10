f7a6467e85fffc1b5219d6a72162b4e630a4c00f
ViniciusQueiroz327 <gm.gb@outlook.com>
2024-09-09 21:24:38 -0300

    a few modifications

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+    alert('Minha primeira página desenvolvida, em 2021, e possui uma responsividade não muito agradável, diferente dos meus projetos SiteMK, MyPokedex e MeuCurriculo, por exemplo. Agradeço a compreensão!')
+})
